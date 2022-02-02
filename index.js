8c5967d235b8fe2093191c16d692910d1bc79897
Sai Arunesh <vssaiarunesh@gmail.com>
2022-02-02 22:12:49 +0530

    cleanup

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const showModalBtn = document.querySelector("#show-modal");
 const modalContainer = document.querySelector("#example-modal");
 
 function showModal() {
-    modalContainer.style.display = "flex";
-  }
-  function closeModal() {
-    modalContainer.style.display = "none";
-  }  
+  modalContainer.style.display = "flex";
+}
+
+function closeModal() {
+  modalContainer.style.display = "none";
+}
 
 showModalBtn.addEventListener("click", showModal);
 modalContainer.addEventListener("click", closeModal);
